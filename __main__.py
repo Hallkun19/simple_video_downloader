47d48f07ceac095a759ca93951c646155feb05d6
はるくん <164134427+Hallkun19@users.noreply.github.com>
2024-07-24 21:54:28 +0900

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -11,11 +11,14 @@ from mimetypes import guess_type
 from threading import Thread
 import re
 import shutil
+import datetime
 import urllib.request, urllib.error
+import glob
 
 DEFAULT_FONT = "游ゴシック"
 DEFAULT_FONT_SIZE = 15
 DEFAULT_FONT_HEIGHT = DEFAULT_FONT_SIZE * 2
+
 
 class App(ctk.CTk):
     
@@ -24,7 +27,7 @@ class App(ctk.CTk):
 
         self.titlefonts = (DEFAULT_FONT, 25, "bold")
         self.fonts = (DEFAULT_FONT, DEFAULT_FONT_SIZE)
-        self.now_version = "0.0.1"
+        self.now_version = "0.0.2"
 
         # チェックボックスの状態を保存するリストを初期化
         self.download_filetype_check = [tk.StringVar(value="0") for _ in range(5)]
@@ -33,6 +36,7 @@ class App(ctk.CTk):
         self.realtime_progress = tk.StringVar()
         self.progress_finish_flag = tk.StringVar()
         self.playlist_download = tk.StringVar(value="0")
+        self.ffmpeg_path = tk.StringVar()
 
         self.setup_form()
 
@@ -77,12 +81,13 @@ class App(ctk.CTk):
     def create_thread(self):
         self.run_thread = Thread( target=self.method_in_a_thread )
         self.run_thread.start()
-        print(self.run_thread)
+        self.export_log(f"create thread:{self.run_thread}")
 
     def method_in_a_thread(self):
         self.download_start()
 
     def download_start(self):
+        self.export_log(f"download_start()")
         self.progress_finish_flag.set("false")
         selected_filetypes = [var.get() for var in self.download_filetype_check]
         selected_filetypes = [s for s in selected_filetypes if s != '0']
@@ -91,15 +96,38 @@ class App(ctk.CTk):
 
         if not download_url:
             self.show_error("URLを入力してください！")
+            self.export_log(f"url error")
             return
 
         if not download_path:
             self.show_error("ダウンロード場所を入力してください！")
+            self.export_log(f"download path error")
             return
 
         if not selected_filetypes:
             self.show_error("ファイル形式を選択してください！")
+            self.export_log(f"format error")
             return
+
+        if os.path.isfile("ffmpeg.exe"):
+            self.ffmpeg_path.set(os.path.realpath(os.path.abspath('ffmpeg.exe')))
+        elif "ffmpeg" in os.environ['Path']:
+            ffmpeg_path = [s for s in os.environ["Path"].split(";") if 'ffmpeg' in s]
+            ffmpeg_path = ffmpeg_path[0] + "\\ffmpeg.exe"
+            print(ffmpeg_path)
+            self.ffmpeg_path.set(ffmpeg_path)
+        else:
+            self.realtime_progress.set("初回準備中...")
+            url = 'https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip'
+            dst_path = 'ffmpeg-master-latest-win64-gpl-shared.zip'
+            urllib.request.urlretrieve(url, dst_path)
+            shutil.unpack_archive(dst_path, 'ffmpeg')
+            shutil.move('ffmpeg/ffmpeg-master-latest-win64-gpl-shared/bin/ffmpeg.exe', '.')
+            os.remove('ffmpeg-master-latest-win64-gpl-shared.zip')
+            shutil.rmtree('ffmpeg/')
+            self.ffmpeg_path.set(os.path.realpath(os.path.abspath('ffmpeg.exe')))
+
+            
 
         self.realtime_progress.set("準備中...")
         self.progress_bar.set(0)
@@ -109,10 +137,13 @@ class App(ctk.CTk):
             for future in concurrent.futures.as_completed(futures):
                 try:
                     future.result()
+                    self.export_log(f"try future.result()")
                 except Exception as exc:
+                    self.export_log(f"Exception occurred: {exc}")
                     print(f"Exception occurred: {exc}")
 
         self.realtime_progress.set("ダウンロード完了！")
+        self.export_log(f"download progress finish")
 
     def progress_hook(self, d):
         if self.progress_finish_flag.get() == "true":
@@ -131,14 +162,23 @@ class App(ctk.CTk):
         playlist_download = self.url_insert_frame.get_checkbox()
         edited_url = url
 
-        if playlist_download == "0":
-            edited_url = edited_url.split("&list=")[0]
+        """
+        
+            self.export_log(f"remove list url")
+            edited_url = edited_url.split("&list=")[0]"""
 
         mime_type, encoding = guess_type(f"example.{file}")
+        ffmpeg_path = self.ffmpeg_path.get()
         option = {
             'outtmpl': os.path.join(path, '%(title)s.%(ext)s'),
-            'progress_hooks': [self.progress_hook]
+            'progress_hooks': [self.progress_hook],
+            'ffmpeg_location':ffmpeg_path
         }
+
+        if playlist_download == "0":
+            option['noplaylist'] = True
+        else:
+            option['noplaylist'] = False
 
         # ダウンロード先ディレクトリの存在を確認し、存在しない場合は作成する
         output_dir = os.path.dirname(option['outtmpl'])
@@ -146,17 +186,23 @@ class App(ctk.CTk):
             os.makedirs(output_dir)
 
         if mime_type.startswith("audio"):
+            self.export_log(f"audio format detect")
             option['format'] = 'bestaudio'
             option['postprocessors'] = [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': file,
             }]
+            with YoutubeDL(option) as ydl:
+                ydl.download(edited_url)
 
         elif mime_type.startswith("video"):
+            self.export_log(f"video format detect")
             with YoutubeDL(option) as ydl:
+                self.export_log(f"get video info")
                 info_dict = ydl.extract_info(edited_url, download=False)
                 video_entries = []
                 if 'entries' in info_dict:
+                    self.export_log(f"entries in info")
                     for entry in info_dict['entries']:
                         video_entries.append(entry)
                 else:
@@ -168,15 +214,18 @@ class App(ctk.CTk):
 
                     if file in available_formats:
                         option['format'] = f'bestvideo*[ext={file}]+bestaudio[ext=m4a]/best[ext={file}]'
+                        self.export_log(f"formats available:{file}")
                     else:
                         option['format'] = f'bestvideo*+bestaudio/best'
                         option['postprocessors'] = [{
                             'key': 'FFmpegVideoConvertor',
                             'preferredformat': file
                         }]
+                        self.export_log(f"formats not available, conert to:{file}")
 
                     try:
                         with YoutubeDL(option) as ydl:
+                            self.export_log(f"try download {video_entry['webpage_url']}")
                             ydl.download([video_entry['webpage_url']])
                             video_file = ydl.prepare_filename(video_entry)
 
@@ -190,14 +239,24 @@ class App(ctk.CTk):
                         os.utime(cleaned_file_path, (current_time, current_time))
                     except Exception as e:
                         print(f"Exception occurred while downloading {video_entry['webpage_url']}: {e}")
+                        self.export_log(f"Exception occurred while downloading {video_entry['webpage_url']}: {e}")
 
         if self.progress_finish_flag.get() == "true":
             self.realtime_progress.set("ダウンロード完了！")
+        self.export_log(f"download_video() finish")
 
 
 
     def show_error(self, messages):
         tmsg.showerror("Error",messages)
+
+    def export_log(self, log):
+        f = open("log.txt", "a", encoding="utf-8")
+        nowtime = datetime.datetime.now()
+        nowtime = nowtime.strftime("%Y-%m-%d %H:%M:%S")
+        
+        f.write(f"[{nowtime}]:{log}\n")
+        f.close
 
 
 class urlSettings(ctk.CTkFrame):
@@ -223,7 +282,7 @@ class urlSettings(ctk.CTkFrame):
         self.textbox.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="ew")
 
         self.button_select = ctk.CTkButton(master=self, height=DEFAULT_FONT_HEIGHT * 1.15, fg_color="transparent", hover_color=("#bcbcbc", "#474747"), border_width=2, text_color=("gray10", "#DCE4EE"), command=self.paste_button, text="ペースト", font=self.fonts)
-        self.button_select.grid(row=1, column=1, padx=10, pady=(0, 10))
+        self.button_select.grid(row=1, column=1, padx=(10,2), pady=(0, 10))
 
         self.checkbox = ctk.CTkCheckBox(self, text="プレイリストとして\nダウンロード(非推奨)", border_width=2, checkbox_height=DEFAULT_FONT_SIZE * 1.5, checkbox_width=DEFAULT_FONT_SIZE * 1.5, variable=self.playlist_download, onvalue="1", offvalue="0", font=(DEFAULT_FONT, 12))
         self.checkbox.grid(row=1, column=2, padx=10, pady=(0, 10))
